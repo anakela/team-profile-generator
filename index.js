c2710622d62c6fda4911db67e1ad4848a40c37e9
Angie <103882157+anakela@users.noreply.github.com>
2022-07-02 13:23:08 -1000

    Updated the questions array in index.js.

--- index.js
+++ index.js
@@ -3,11 +3,42 @@ const fs = require('fs');
 
 // WHEN I am prompted for my team members and their information
 inquirer
+    // WHEN I start the application
+    // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
     .prompt([
         {
-            message: '',
-            name: '',
+            message: 'What is your manager name?',
+            name: 'manager-name',
             type: 'input',
+            default: 'Team Manager'
+        },
+        {
+            message: 'What is your employee ID?',
+            name: 'id',
+            type: 'input',
+            default: '1'
+        },
+        {
+            message: 'What is your email address?',
+            name: 'email',
+            type: 'input',
+            default: 'example@example.com'
+        },
+        {
+            message: 'What is your office number?',
+            name: 'off-num',
+            type: 'input',
+            default: '1'
+        },
+        {
+            message: 'Would you like to add an engineer or intern?',
+            name: 'add-eng-int',
+            type: 'rawlist',
+            choices: [
+                'Yes, add engineer',
+                'Yes, add intern',
+                'No, thanks.'
+            ]
         },
     ]);
 
